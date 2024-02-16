2815a62c158e315779437edf2d664cb154d8b993
Rob <rob_heijligers@hotmail.com>
2024-02-16 20:39:33 +0100

    Create RenameFolder.py

---
+++ RenameFolder.py
@@ -0,0 +1,144 @@
+# Rename Images with Date Photo Taken
+
+# Purpose: Renames image files in a folder based on date photo taken from EXIF metadata
+
+# Author: Matthew Renze
+
+# Usage: python.exe rename.py input-folder
+#   - input-folder = (optional) the directory containing the image files to be renamed
+
+# Examples: python.exe rename.py C:\Photos
+#           python.exe rename.py
+
+# Behavior:
+#  - Given a photo named "Photo Apr 01, 5 54 17 PM.jpg"  
+#  - with EXIF date taken of "4/1/2018 5:54:17 PM"  
+#  - when you run this script on its parent folder
+#  - then it will be renamed "20180401-175417.jpg"
+
+# Notes:
+#   - For safety, please make a backup of your photos before running this script
+#   - Currently only designed to work with .jpg, .jpeg, and .png files
+#   - If you omit the input folder, then the current working directory will be used instead.
+
+# Import libraries
+import os
+import sys
+from datetime import datetime
+from PIL import Image
+
+# Set list of valid file extensions
+valid_extensions = [".jpg", ".JPG", ".jpeg", ".JPEG", ".png", ".PNG"]
+
+# If folder path argument exists then use it
+# Else use the current running folder
+#if len(sys.argv) > 1:
+#    folder_path = input_file_path = sys.argv[1]
+#else:
+#    folder_path = os.getcwd()
+
+# Get all files from folder
+root_folder_path = r'\\ZIMA\ZimaShare\Afbeeldingen\Fotos All Years'
+root_folder = os.listdir(root_folder_path)
+
+
+
+# For each file
+for folder_name in root_folder:
+    
+    current_subfolder_name = os.path.join(root_folder_path, folder_name)
+
+    if os.path.isfile(current_subfolder_name):
+        continue
+
+    files = os.listdir(current_subfolder_name)
+
+    date_taken = ""
+
+    failedAttempts = 5
+
+    for file_name in files:
+
+        # Get the file extension
+        file_ext = os.path.splitext(file_name)[1]
+
+        # Skip files without a valid file extension
+        if (file_ext not in valid_extensions):
+            continue
+
+        # Create the old file path
+        old_file_path = os.path.join(current_subfolder_name, file_name)
+
+        # Open the image
+        image = Image.open(old_file_path)
+
+        # Get the EXIF metadata
+        metadata = image._getexif()
+
+        # Close the image
+        image.close()
+
+        # Check if the metadata exists
+        if metadata is None:
+            print(f"EXIF metadata not found in file: {folder_name}")
+            failedAttempts -= 1
+            if failedAttempts != 0:
+                continue
+
+
+        if metadata is not None:
+            # Get the date taken from the metadata
+            if 36867 in metadata.keys():
+                date_taken = metadata[36867]
+            elif 306 in metadata.keys():
+                date_taken = metadata[306]
+            else:
+                print(f"Date not found in file: {old_file_path}")     
+                failedAttempts -= 1
+                if failedAttempts != 0:
+                    continue
+        
+           
+        if date_taken == '0000:00:00 00:00:00':
+            failedAttempts -= 1
+            if failedAttempts != 0:
+                continue
+            
+        if failedAttempts == 0:
+            dateobj= os.path.getmtime(old_file_path)
+            date_taken =  datetime.fromtimestamp(dateobj)
+            date_taken = date_taken.__format__("%Y:%m:%d %H:%M:%S")
+            #date_taken = os.path.getctime(old_file_path)
+
+        
+
+        # Get the date taken as a datetime object
+        date_taken = datetime.strptime(date_taken, "%Y:%m:%d %H:%M:%S")
+
+        # Reformat the date taken to "YYYYMMDD-HHmmss"
+        # NOTE: Change this line to change the date/time format of the output filename
+        #date_time = date_taken.strftime("%Y%m%d-%H%M%S")
+        year_taken = date_taken.strftime("%Y")
+    
+        # we have what we need
+        break    # break here
+
+    if len(year_taken ) > 0:
+        if folder_name.startswith(year_taken):
+            continue
+
+        new_folder_name = year_taken + " " + folder_name
+        new_folder_path = os.path.join(root_folder_path, new_folder_name)
+
+        if os.path.exists(new_folder_path):
+            os.rename(new_folder_path, new_folder_path + ' - DUPLICATE NAME')
+            print(f"found already excisting name: {new_folder_name} added : DUPLICATE NAME to new foldername")
+        else:
+            os.rename(current_subfolder_name, new_folder_path)
+            print(f"renamed: {folder_name} to {new_folder_name} in {root_folder_path}")
+        
+        # Create the new folder path
+        #new_file_path = os.path.join(folder_path, new_file_name)
+
+    # Rename the file
+    #os.rename(old_file_path, new_file_path)
